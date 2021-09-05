0ec6f18085d8b9dd07c54d6ee3064e773885bafe
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-05 02:53:37 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,171 @@
+// var userName=prompt("whats your name?")
+
+
+// var h1=document.getElementById("greet");
+// // console.log(h1.innerHTML)
+
+
+// h1.innerHTML="wellcome " + userName;
+
+
+// function statement(){
+//     var pro=prompt("Message");
+//     var id=document.getElementById("p");
+    
+//         id.innerHTML=pro
+    
+// }
+
+
+
+
+
+
+
+// function state(){
+
+
+
+// var age=+prompt("Enter Your Age!");
+// var p=document.getElementById("p")
+
+
+// if (age>18) {
+//     p.innerHTML="You are eligible for classes"
+// }
+
+// else{
+//     p.innerHTML="You are not eligible for classes"
+// }
+
+
+
+
+// }
+
+
+
+
+// function state(){
+//     var pro=+prompt("Enter age")
+//     var p=document.getElementById("p");
+
+//     if (pro>18) {
+// p.innerHTML="yes"
+        
+//     }
+//     else{
+// p.innerHTML="nos"
+
+//     }
+// }
+
+
+
+
+
+
+
+
+
+// function age(){
+//     var userAge = +prompt("Enter Age");
+
+
+//     var para=document.getElementById("para");
+
+//     if (userAge>14) {
+//         para.innerHTML="You are eligible for classes"
+//     }
+//     else{
+//         para.innerHTML="You are not eligible"
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+// var age = +prompt("What is your age?")
+
+
+// if (age === 18) {
+//     console.log("Your are Eligible")
+// }
+
+
+
+
+// var religion="islam";
+
+// var pro=prompt("Enter Religion")
+// if (pro !== "islam") {
+//     console.log("Your are not eligible")
+// }
+
+
+
+
+// let counter =(()=>{
+//     let count=0;
+//     return {
+//         increment:()=>{
+//             return count +1;
+//         },
+//         decrement:()=>{
+//             return count -1
+//         }
+//     }
+// })();
+
+// console.log(counter.increment());
+// console.log(counter.increment());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
